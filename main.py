7312ca38ef0eab8200983302ecd687c63c758dd0
Muhammadkaj11 <muhdkaj11@gmail.com>
2024-12-06 21:06:11 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+SPCMcountry_code={'India':'0091',
+                  'pustralia':'0025',
+                  'Nepal':'00977'}
+print("country_code for India-")
+print(SPCMcountry_code.get('India','Not found'))
+
+print("country_code for japan-")
+print(SPCMcountry_code.get('japan','Not found'))
